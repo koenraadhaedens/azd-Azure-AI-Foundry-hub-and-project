be39b7dc121cf8ab26cf735c450386ec6484dce1
Uwe Baumann <uwe.baumann@hotmail.de>
2026-02-04 14:52:19 +0100

    Convertet demo code to use DefaultAzureCredential() and dotnet Core 8

--- Demos/AI-3002/mslearn-ai-info-extraction-AI-3002-Develop-AI-information-extraction-solutions-in-Azure-AI-102-Day-5-main/Labfiles/prebuilt-doc-intelligence/c-sharp/Program.cs
+++ Demos/AI-3002/mslearn-ai-info-extraction-AI-3002-Develop-AI-information-extraction-solutions-in-Azure-AI-102-Day-5-main/Labfiles/prebuilt-doc-intelligence/c-sharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Azure;
 using Azure.Identity;
+using Azure.AI.FormRecognizer.DocumentAnalysis;
 using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
@@ -15,7 +16,7 @@ namespace document_analysis
         {
             // Clear the console
             Console.Clear();
-            
+
             try
             {
                 // Get config settings
@@ -32,18 +33,46 @@ namespace document_analysis
                 Console.WriteLine("\nConnecting to Forms Recognizer at: {0}", endpoint);
                 Console.WriteLine("Analyzing invoice at: {0}\n", fileUri.ToString());
 
-
-
                 // Create the client
+                var client = new DocumentAnalysisClient(new Uri(endpoint), credential);
 
-
-                // Analyze the invoice
-
+                // Analyse the invoice
+                AnalyzeDocumentOperation operation = await client.AnalyzeDocumentFromUriAsync(
+                    WaitUntil.Completed,
+                    "prebuilt-invoice", fileUri);
 
                 // Display invoice information to the user
+                AnalyzeResult result = operation.Value;
 
+                foreach (AnalyzedDocument invoice in result.Documents)
+                {
+                    if (invoice.Fields.TryGetValue("VendorName", out DocumentField? vendorNameField))
+                    {
+                        if (vendorNameField.FieldType == DocumentFieldType.String)
+                        {
+                            string vendorName = vendorNameField.Value.AsString();
+                            Console.WriteLine($"Vendor Name: '{vendorName}', with confidence {vendorNameField.Confidence}.");
+                        }
+                    }
 
-                
+                    if (invoice.Fields.TryGetValue("CustomerName", out DocumentField? customerNameField))
+                    {
+                        if (customerNameField.FieldType == DocumentFieldType.String)
+                        {
+                            string customerName = customerNameField.Value.AsString();
+                            Console.WriteLine($"Customer Name: '{customerName}', with confidence {customerNameField.Confidence}.");
+                        }
+                    }
+
+                    if (invoice.Fields.TryGetValue("InvoiceTotal", out DocumentField? invoiceTotalField))
+                    {
+                        if (invoiceTotalField.FieldType == DocumentFieldType.Currency)
+                        {
+                            CurrencyValue invoiceTotal = invoiceTotalField.Value.AsCurrency();
+                            Console.WriteLine($"Invoice Total: '{invoiceTotal.Symbol}{invoiceTotal.Amount}', with confidence {invoiceTotalField.Confidence}.");
+                        }
+                    }
+                }
             }
             catch (Exception ex)
             {
@@ -55,4 +84,4 @@ namespace document_analysis
     }
 }
 
-            
+
