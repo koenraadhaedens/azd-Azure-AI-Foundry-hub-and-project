cde6967efcb214a433b1022b87500a96ba5e7dc5
koenraadhaedens <koenraad@khd.be>
2026-05-10 18:30:28 +0000

    mi app

--- Demos/AI-3003/mslearn-ai-language-main/Labfiles/02-qna/Python/qna-app/qna-app.py
+++ Demos/AI-3003/mslearn-ai-language-main/Labfiles/02-qna/Python/qna-app/qna-app.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
 import os
 
-# Import namespaces
+# import namespaces
+from azure.identity import DefaultAzureCredential
+from azure.ai.language.questionanswering import QuestionAnsweringClient
 
 
 def main():
@@ -9,14 +11,27 @@ def main():
         # Get Configuration Settings
         load_dotenv()
         ai_endpoint = os.getenv('AI_SERVICE_ENDPOINT')
-        ai_key = os.getenv('AI_SERVICE_KEY')
         ai_project_name = os.getenv('QA_PROJECT_NAME')
         ai_deployment_name = os.getenv('QA_DEPLOYMENT_NAME')
 
-        # Create client using endpoint and key
-
+        # Create client using managed identity
+        credential = DefaultAzureCredential()
+        ai_client = QuestionAnsweringClient(endpoint=ai_endpoint, credential=credential)
 
         # Submit a question and display the answer
+        # Submit a question and display the answer
+        user_question = ''
+        while True:
+            user_question = input('\nQuestion:\n')
+            if user_question.lower() == "quit":                
+                break
+            response = ai_client.get_answers(question=user_question,
+                                            project_name=ai_project_name,
+                                            deployment_name=ai_deployment_name)
+            for candidate in response.answers:
+                print(candidate.answer)
+                print("Confidence: {}".format(candidate.confidence))
+                print("Source: {}".format(candidate.source))
 
 
 
